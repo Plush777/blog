4c43b8e11e9ddbbae1ea9a1df96a30e79f739556
Plush777 <sky11916@naver.com>
2024-01-28 18:36:18 +0900

    post 카테고리 작업중

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,11 @@
 export const SITE = {
     desc: "경험과 노하우를 공유하고 있어요.",
     title: "Plush 기술 블로그",
+    name: "Plush",
     ogImage: "미정",
     ogType: "website",
     twitterCard: "summary",
     robots: "index,follow",
+    github: "https://github.com/Plush777",
+    discord: "https://discord.gg/ewbRavVv"
 };
