0fbde23b08eaccbfac93e3de587cb3c21e574992
Plush777 <sky11916@naver.com>
2024-01-30 09:40:19 +0900

    mdx

---
+++ src/utils/getRandomColor.js
@@ -0,0 +1,6 @@
+export default function getRandomColor() {
+    const colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple'];
+    const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+    return randomColor;
+}
