a5cdaf35c8b4a3edf83360ccaabfbaf5d39884dd
Plush777 <sky11916@naver.com>
2024-02-07 16:22:38 +0900

    base head 수정

--- src/consts.ts
+++ src/consts.ts
@@ -2,10 +2,11 @@ export const SITE = {
     desc: "배운 모든 것을 기록하고 정리하는 블로그입니다.",
     title: "Plush 기술 블로그",
     name: "Plush",
-    ogImage: "미정",
+    ogImage: "/og.png",
     ogType: "website",
     twitterCard: "summary",
     robots: "index,follow",
     github: "https://github.com/Plush777",
-    discord: "https://discord.gg/ewbRavVv"
+    discord: "https://discord.gg/ewbRavVv",
+    url: "https://plush-tech.netlify.app"
 };
