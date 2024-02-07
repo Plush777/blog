1582ef81d1018cb390a0004bd9daff5be8a517df
Plush777 <sky11916@naver.com>
2024-02-07 16:37:33 +0900

    og image

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const SITE = {
     desc: "배운 모든 것을 기록하고 정리하는 블로그입니다.",
     title: "Plush 기술 블로그",
     name: "Plush",
-    ogImage: "/og.png",
+    ogImage: "og.png",
     ogType: "website",
     twitterCard: "summary",
     robots: "index,follow",
