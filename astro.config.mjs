dd773b57c82f64b1a2a65436413e68629069a16d
Plush777 <sky11916@naver.com>
2025-06-09 20:56:15 +0900

    pwa 캐시문제 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 // https://www.freecodecamp.org/news/how-to-add-google-analytics-to-your-astro-website/
@@ -10,18 +9,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
-
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-        debug: true,
-        serviceWorker: {
-          cache: false,
-          skipWaiting: true,
-          clientsClaim: true
-        }
-      },
-    }),
   ],
   output: 'static',
   vite: {
